349a23c452ec01abcda8b561861c26e47c5a6162
Eugene <istrilet@sfu.ca>
2022-09-28 02:26:44 -0700

    checkes in Head

--- client/pages/api/mail.js
+++ client/pages/api/mail.js
@@ -1,22 +1,22 @@
-// const mail = require('@sendgrid/mail');
+const mail = require('@sendgrid/mail');
 
-// mail.setApiKey(process.env.SENDGRID_API_KEY);
+mail.setApiKey(process.env.SENDGRID_API_KEY);
 
-// export default async (req, res) => {
-//   const body = JSON.parse(req.body);
+export default async (req, res) => {
+  const body = JSON.parse(req.body);
 
-//   const message = `
-//     Name: ${body.name}\r\n
-//     Email: ${body.email}\r\n
-//     Message: ${body.message}
-//   `;
+  const message = `
+    Name: ${body.name}\r\n
+    Email: ${body.email}\r\n
+    Message: ${body.message}
+  `;
 
-//   await mail.send({
-//     to: 'eugenearcher3@gmail.com',
-//     subject: 'New Message!',
-//     text: message,
-//     html: message.replace(/\r\n/g, '<br>'),
-//   });
+  await mail.send({
+    to: 'eugenearcher3@gmail.com',
+    subject: 'New Message!',
+    text: message,
+    html: message.replace(/\r\n/g, '<br>'),
+  })
 
-//   res.status(200).json({ status: 'Ok' });
-// }
+  res.status(200).json({ status: 'Ok' });
+}
