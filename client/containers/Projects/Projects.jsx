4449735ea3c258deacd7f1f92f21b4cdd3e51a0f
Eugene <istrilet@sfu.ca>
2022-10-01 23:25:05 -0700

    changes in project page

--- client/containers/Projects/Projects.jsx
+++ client/containers/Projects/Projects.jsx
@@ -18,6 +18,45 @@ import {
   SiCss3
 } from 'react-icons/si';
 
+const projectData = [
+  {
+    name: 'Path Finder',
+    description:
+      'The main goal of this project is to identify the most optimal path finding algorithm' +
+      'for different types of labyrinths. Implemented seven path finding algorithms and' +
+      'tested them over a set of various labyrinths to identify the most optimal algorithm for a' +
+      'particular type of labyrinths.',
+    styles: 'item2',
+    frontEndIcons: [
+      <SiJavascript size='2rem' />,
+      <SiNextdotjs size='2rem' />,
+      <SiCss3 size='2rem' />,
+      <SiRedux size='2rem' />,
+    ],
+  },
+  {
+    name: 'Unifyi',
+    description:
+      'A full-stack web app with the primary focus on improving university' +
+      'experiences for new and existing university students. Implemented' +
+      'within microservices approach and transactional outbox pattern',
+    styles: 'item3',
+  },
+  {
+    name: 'Equity Sentiment',
+    description:
+      'Social sentiment analysis Trends and volatility in social interest regarding public companies',
+    styles: 'item4',
+  },
+  {
+    name: 'Web3 Collection',
+    description:
+      'Collection that would allow the collector to swap the properties of regular NFT' +
+      'and combine with newly created NFT objects',
+    styles: 'item5',
+  },
+];
+
 export default function Projects() {
   return (
     <main className={styles['grid']}>
@@ -25,7 +64,21 @@ export default function Projects() {
         <h1>Personal Projects</h1>
         <p>Done with love, dedication and</p>
       </CardComponent>
-      <CardComponent newStyles={`${styles['card']} ${styles['item2']}`}>
+      {projectData.map((item, idx) => (
+        <CardComponent key={idx} newStyles={`${styles['card']} ${styles[item.styles]}`}>
+          <div className={styles['card__header-container']}>
+            <h1>{item.name}</h1>
+            <div />
+            <SiYoutube size='2rem' className={styles['card__youtube-log']} />
+          </div>
+          <p className={styles['card__text-p']}>{item.description}</p>
+          <div className={styles['container__tech-stack']}>
+            <h3>Tech stack</h3>
+            <div className={styles['tech-stack']}>{item.frontEndIcons}</div>
+          </div>
+        </CardComponent>
+      ))}
+      {/* <CardComponent newStyles={`${styles['card']} ${styles['item2']}`}>
       <div className={styles['card__header-container']}>
           <h1>Path Finder</h1>
           <div />
@@ -90,7 +143,7 @@ export default function Projects() {
       <CardComponent newStyles={`${styles['card']} ${styles['item4']}`}>
         <h2>because interactions make it more fun</h2>
       </CardComponent>
-      <CardComponent newStyles={`${styles['card']} ${styles['item5']}`}></CardComponent>
+      <CardComponent newStyles={`${styles['card']} ${styles['item5']}`}></CardComponent> */}
     </main>
   );
 }
--- client/pages/api/mail.js
+++ client/pages/api/mail.js
@@ -2,7 +2,7 @@ const mail = require('@sendgrid/mail');
 
 mail.setApiKey(process.env.SENDGRID_API_KEY);
 
-export default async (req, res) => {
+export default (req, res) => {
   const body = JSON.parse(req.body);
 
   const message = `
@@ -10,13 +10,52 @@ export default async (req, res) => {
     Email: ${body.email}\r\n
     Message: ${body.message}
   `;
-
-  await mail.send({
-    to: 'eugenearcher3@gmail.com',
-    subject: 'New Message!',
+  
+  const data = {
+    to: 'yevstrilets@gmail.com',
+    from: 'hello@yevstrilets.com',
+    subject: 'New message!',
     text: message,
     html: message.replace(/\r\n/g, '<br>'),
-  })
+  };
+  mail.send(data);
 
   res.status(200).json({ status: 'Ok' });
-}
+};
+
+// require('dotenv').config()
+
+// const mail = require('@sendgrid/mail')
+
+// const { SENDGRID_API_KEY, FROM_EMAIL, TO_EMAIL } = process.env
+
+// mail.setApiKey(process.env.SENDGRID_API_KEY);
+
+// export default async function handler(req, res) { 
+//     const { name, email, message } = req.body;
+//     const msg = { 
+//         to: TO_EMAIL,
+//         from: FROM_EMAIL,
+//         subject: 'Nextjs Contact form',
+//         html: `<p><strong>Name:</strong>${name}</p>
+//         <p><strong>Email:</strong>${email}</p>
+//         <p><strong>Message:</strong>${message}</p>
+//         `
+//     }
+//     await mail.send(msg)
+//     res.json({success: true});
+//     try {
+//         await mail.send(data);
+//         res.status(200).json({ status: 'OK' });
+//       } catch (error) {
+//         console.log(error);
+//         if (error.response) {
+//           console.log(error.response.body);
+//         }
+//         res.status(400).json({ status: "ERROR", message: error.message });
+//       }
+// }
+
+// export default (req, res) => { 
+//     res.status(200).json({status: 'Ok'})
+// }
