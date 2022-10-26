184d6250af7dd5a86526ad06a23d9e0bf63a6c3c
Eugene <istrilet@sfu.ca>
2022-10-26 00:08:41 -0700

    constact form

--- client/components/contactForm/ContactForm.jsx
+++ client/components/contactForm/ContactForm.jsx
@@ -3,80 +3,132 @@ import styles from './form.module.css'
 import ContactFormBtn from './ContactFormBtn';
 
 export default function ContactForm() {
-  //  const [values, setValues] = useState({
-  //   name: '',
-  //   email: '',
-  //   message: '',
-  //  })
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-  //  const {name, email, message} = values
+   //   Form validation state
+   const [errors, setErrors] = useState({});
 
-    // async function handleOnSubmit(e) {
-    //   e.preventDefault();
+   //   Setting button text on form submission
+   const [buttonText, setButtonText] = useState("Send");
+ 
+   // Setting success or failure messages states
+   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+   const [showFailureMessage, setShowFailureMessage] = useState(false);
+ 
+   // Validation check method
+   const handleValidation = () => {
+     let tempErrors = {};
+     let isValid = true;
+ 
+     if (fullname.length <= 0) {
+       tempErrors["fullname"] = true;
+       isValid = false;
+     }
+     if (email.length <= 0) {
+       tempErrors["email"] = true;
+       isValid = false;
+     }
+     if (message.length <= 0) {
+       tempErrors["message"] = true;
+       isValid = false;
+     }
+ 
+     setErrors({ ...tempErrors });
+     console.log("errors", errors);
+     return isValid;
+   };
 
-    //   const formData = {};
+   const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    //   Array.from(e.currentTarget.elements).forEach((field) => {
-    //     if (!field.name) return;
-    //     formData[field.name] = field.value;
-    //   });
+    let isValidForm = handleValidation();
 
-    //   await fetch('/api/mail', {
-    //     method: 'POST',
-    //     body: JSON.stringify(formData),
-    //   });
-    // }
+    if (isValidForm) {
+      setButtonText("Sending");
+      const res = await fetch("/api/mail", {
+        body: JSON.stringify({
+          email: email,
+          fullname: fullname,
+          message: message,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+      });
 
-    // const handleOnSubmit = async e => { 
-    //   e.preventDefault();
-    //   try { 
-    //     await fetch('/client/pages/api/mail.js', {
-    //       method: 'POST',
-    //       headers: {
-    //         "Content-Type" : 'application/json'
-    //       },
-    //       body: JSON.stringify(values)
-    //     })
-    //   } catch(error) { 
-    //     console.log(error)
-    //     if (error.response) {
-    //       console.log(error.response.body);
-    //     }
-    //   }
-    // }
-
-  // const handleChange = e => setValues({...values, [e.target.name]: e.target.value})
+      const { error } = await res.json();
+      if (error) {
+        console.log(error);
+        setShowSuccessMessage(false);
+        setShowFailureMessage(true);
+        setButtonText("Send");
+        return;
+      }
+      setShowSuccessMessage(true);
+      setShowFailureMessage(false);
+      setButtonText("Send");
+    }
+    console.log(fullname, email, message);
+  };
 
   return (
     <div className={styles['form__container']}>
-      <form method='post' className={styles['form__wrapper']}>
+      <form
+        method='post'
+        onSubmit={handleSubmit}
+        className={styles['form__wrapper']}
+      >
         <input
           type='text'
           className={styles['form__input']}
           placeholder='Name*'
           name='name'
-          // value={name}
-          //  onChange={handleChange}
+          required
+          value={fullname}
+          onChange={(e) => {
+            setFullname(e.target.value);
+          }}
         />
         <input
           type='email'
+          value={email}
+          required
           className={styles['form__input']}
           placeholder='Email*'
           name='email'
-          // value={email}
-          // onChange={handleChange}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
         />
         <textarea
-          className={styles['form__input']}
+          className={styles['form__input-message']}
+          value={message}
+          required
           placeholder='Message*'
           maxLength='1000'
-          // onChange={handleChange}
           name='message'
-          // value={message}
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
         ></textarea>
-      {/* <button className={styles['form__button-submit']}>Contact!</button> */}
+        <button className={styles['form__button-submit']}>{buttonText}</button>
+        {/* <ContactFormBtn /> */}
+        <div className='text-left'>
+          {showSuccessMessage && (
+            <p className='text-green-500 font-semibold text-sm my-2'>
+              Thankyou! Your Message has been delivered.
+            </p>
+          )}
+          {showFailureMessage && (
+            <p className='text-red-500'>
+              Oops! Something went wrong, please try again.
+            </p>
+          )}
+        </div>
       </form>
-        <ContactFormBtn />
     </div>
   );
 }
